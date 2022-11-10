328b85884b83955bd85b248c88977927b2a55ffc
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-11-10 17:24:42 +0100

    added player movement and input

--- src/player.rs
+++ src/player.rs
@@ -1,11 +1,15 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
+use crate::{components::{Player, Velocity}, TIME_STEP, BASE_SPEED};
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system);
+            .add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
+            .add_system(player_keyboard_event_system)
+            .add_system(player_movement_system);
         
     }
 }
@@ -18,5 +22,30 @@ fn player_spawn_system(mut commands: Commands,
         mesh: meshes.add(shape::Circle::new(50.).into()).into(),
         material: materials.add(ColorMaterial::from(Color::PURPLE)),
         ..default()
-    });
+    })
+    .insert(Player)
+    .insert(Velocity {x: 0., y:0.});
+}
+
+fn player_keyboard_event_system(
+    kb: Res<Input<KeyCode>>,
+    mut query: Query<&mut Velocity, With<Player>>,
+) {
+    if let Ok(mut velocity) = query.get_single_mut() {
+        velocity.x = if kb.pressed(KeyCode::Left) {
+            -1.
+        } else if kb.pressed(KeyCode::Right) {
+            1.
+        } else {
+            0.
+        }
+    }
+}
+
+fn player_movement_system(mut query: Query<(&Velocity, &mut Transform), With<Player>>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        let translation = &mut transform.translation;
+        translation.x += velocity.x * TIME_STEP * BASE_SPEED;
+        translation.y += velocity.y * TIME_STEP * BASE_SPEED;
+    }
 }
